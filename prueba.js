6ae6e9bd50a7158138157d7e02e73e5472499892
belenaguilarv <belenaguiilarv@gmail.com>
2023-05-20 22:50:52 -0300

    Falta completar Desafio

--- prueba.js
+++ prueba.js
@@ -1,43 +1,15 @@
-class Contador{
-    static cuentaGlobal = 0; // pertenece directamente a la clase
-    constructor(){
-        this.nombreResponsable = "Belu"; 
-        this.cuenta = 0;  
-    }
-    /* entonces cada vez que cree un contador va a tener 
-    un nombreResponsable y cuenta
-*/
-    getResponsable(){
-        console.log(this.nombreResponsable);
-        return this.nombreResponsable; // el this indica de donde sale ese dato
-    }
-    getCuentaIndividual(){
-        console.log(this.cuenta);
-        return this.cuenta; 
-    }
-    sumarCuenta(){
-        this.cuenta++;
-        Contador.cuentaGlobal++;
-    }
-    
-}
+let edad = 30;
+let nombre = "Maria";
+let segundoNombre = "";
 
-const belu = new Contador("belu");
-const flor = new Contador("flor");
-const agus = new Contador("agus");
+let edadFinal = edad ?? 0; // ?? pone el resultado de la izq o de la der
+let nombreFinal = nombre ?? "amiga";
+let segundoNombreFinal = segundoNombre ?? "falta ingresar segundo nombre";
 
-belu.sumarCuenta();
-belu.sumarCuenta();
-flor.sumarCuenta();
-agus.sumarCuenta();
+//console.log(edad, nombre, segundoNombre);
+console.log(segundoNombreFinal); // devuleve vacio porque esta ??
 
 
-console.log(belu.cuenta);
-console.log(Contador.cuentaGlobal);
-
-
-//belu.getResponsable();
-//belu.getCuentaIndividual();
-
-
+// || -> pone el mensaje "falta ingresar segundo nombre"
+// ?? -> deja vacio
 
