0f728252f11be975320bf4e14f4db164c9e85d32
belenaguilarv <belenaguiilarv@gmail.com>
2023-05-27 12:27:53 -0300

    Update

--- Desafio4/src/ProductManager.js
+++ Desafio4/src/ProductManager.js
@@ -1,37 +1,76 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 
 class ProductManager {
-  constructor() {
-    this.filePath = '../data/products.json';
+  constructor(path) {
+    this.path = path;
   }
 
-  async getProducts(limit = null) {
+  getProducts() {
     try {
-      const data = await fs.readFile(this.filePath);
-      let products = JSON.parse(data);
-
-      if (limit !== null) {
-        products = products.slice(0, limit);
-      }
-
-      return products;
+      const products = fs.readFileSync(this.path, 'utf-8');
+      return JSON.parse(products);
     } catch (err) {
-      console.error(`Error reading file: ${err}`);
-      return null;
+      console.log('Error al leer el archivo de productos: ', err);
+      return [];
     }
   }
 
-  async getProductById(productId) {
-    try {
-      const data = await fs.readFile(this.filePath);
-      const products = JSON.parse(data);
-      const product = products.find(p => p.id == productId);
+  /**
+    Metodo que debe buscar en el arreglo el producto que coincida con 
+    el id
+    @param {number} id - El identificador del producto que se esta buscando 
+    @returns {object / null} - Objeto con todas las propiedades del producto 
+                              si es encontrado, de lo contrario retorna null.
+  */
+  getProductById(id) {
+    const products = this.getProducts();
+    const product = products.find((p) => p.id === id);
+    return product;
+  }
 
-      return product;
-    } catch (err) {
-      console.error(`Error reading file: ${err}`);
-      return null;
+/**
+    Metodo que debe actualiza los valores en el arreglo el producto que coincida con 
+    el id
+    @param {number} id - El identificador del producto que se esta buscando 
+    @param {object} fieldsToUpdate - Los campos a actualizar 
+    @returns {object / null} - Devuelve el objeto "updatedProduct" si se 
+    actualiza correctamente o "null" si el producto no se encuentra en la lista.
+  */
+  updateProduct(id, fieldsToUpdate) {
+    const products = this.getProducts();
+    const productIndex = products.findIndex((p) => p.id === id); // para encontrar el id del producto que deseo actualizar
+    if (productIndex !== -1) {      // si esta en la lista creo un nuevo producto
+      const updatedProduct = {
+        ...products[productIndex],
+        ...fieldsToUpdate,
+        id,
+      };
+      products[productIndex] = updatedProduct;
+      this._saveProducts(products);
+      return updatedProduct;
     }
+    return null;
+  }
+
+
+  /**
+    Metodo que elimina el arreglo el producto que coincida con el id
+    @param {number} id - El identificador del producto que se esta buscando 
+  */
+  deleteProduct(id) {
+    let products = this.getProducts();              // obtengo todos los productos
+    products = products.filter((p) => p.id !== id); // filtro para eliminar los que tengan el mismo id
+    this._saveProducts(products);                   // guardo los productos que quedan
+  }
+
+/** 
+  Método privado de la clase ProductManager que se encarga de guardar los productos en un archivo 
+  utilizando el módulo fs de Node.js. 
+  @param {object} - Arreglo con los productos 
+*/
+  _saveProducts(products) {
+    const productsString = JSON.stringify(products, null, 2); // convierte el arreglo a formato JSON
+    fs.writeFileSync(this.path, productsString); //escribir el archivo en la ruta especificada en la propiedad this.path de la instancia de ProductManager.
   }
 }
 
--- Desafio4/src/app.js
+++ Desafio4/src/app.js
@@ -1,33 +1,13 @@
-const express = require('express');
-const ProductManager = require('./ProductManager');
+const express = require("express");
 
-const app = express();
-const port = 3000;
+const app = express()
+app.listen(8080,() => console.log("Server en puerto 8080"))
 
-const productManager = new ProductManager();
+const cartsRouter = require("./routes/cartsRouter")
+const productsRouter = require("./routes/productsRouter")
 
-app.get('/products', async (req, res) => {
-  const limit = req.query.limit || null;
-  const products = await productManager.getProducts(limit);
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-  if (products !== null) {
-    res.json(products);
-  } else {
-    res.sendStatus(500);
-  }
-});
-
-app.get('/products/:pid', async (req, res) => {
-  const productId = req.params.pid;
-  const product = await productManager.getProductById(productId);
-
-  if (product !== null) {
-    res.json(product);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+app.use("/carts", cartsRouter)
+app.use("/products", productsRouter)
